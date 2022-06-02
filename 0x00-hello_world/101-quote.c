4d95d8296089bd2ed26fe48a31fde1546cf716fd
zeezatcoder <azeezatmaja@gmail.com>
2022-06-02 15:14:16 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: Always 1(Success)
  */
 int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	Write(2, str1, 59);
+	write(2, str1, 59);
 	return (1);
 }
