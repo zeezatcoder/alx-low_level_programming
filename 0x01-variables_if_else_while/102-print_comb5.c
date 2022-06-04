d4bf69e1b004f2b22c31619b26534c6e91678888
zeezatcoder <azeezatmaja@gmail.com>
2022-06-03 18:18:20 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,16 +15,16 @@ int main(void)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
-			putchar(',');
+			putchar(' ');
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
-		if (num1 == 98 && num2 == 99)
-			continue;
-
-		putchar(',');
-		putchar(' ');
-
 	}
 
 	putchar('\n');
