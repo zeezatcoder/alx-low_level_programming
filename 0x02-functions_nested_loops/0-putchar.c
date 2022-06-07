35c343f084c0fbd173ad9437489139ac0622336e
zeezatcoder <azeezatmaja@gmail.com>
2022-06-07 08:00:08 -0700

    Write a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char zeezat[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(zeezat[i]);
+
+	}
+
+	_putchar('\n');
+	return (0);
+
+}
