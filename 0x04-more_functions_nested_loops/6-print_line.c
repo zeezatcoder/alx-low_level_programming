08efacdb9ac9e7e1c022a77fd5fe2740d4eacbf3
zeezatcoder <azeezatmaja@gmail.com>
2022-06-09 14:23:44 -0700

    task6.

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,17 +9,11 @@
 
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i;
+	int i;
 
-		for (i = 1; i <= n; i++)
-		{
-			putchar('');
-		}
-		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
 	}
+	_putchar('\n');
 }
