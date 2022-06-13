59822337dc96d2a2a004683c0f2d1c591dd9baa1
zeezatcoder <azeezatmaja@gmail.com>
2022-06-13 08:00:31 -0700

    fix  error.

---
+++ 6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints string in even intervals
+ * @s: the string to print
+ *
+ * Return: void
+ */
+void puts2(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (i % 2 == 0)
+			_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
