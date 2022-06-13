dbc65eab78f2ea187d99ed2178394e4eebac0779
zeezatcoder <azeezatmaja@gmail.com>
2022-06-13 07:26:49 -0700

    task6.

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 -  prints every other character of a string
+ * @str: the string to print
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		_putchar(*(str + i));
+		i = i + 2;
+	}
+	_putchar('\n');
+
+
+}
