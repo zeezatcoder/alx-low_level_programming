a0cd8f535b64b6c885fef72c1645cab7381151c3
zeezatcoder <azeezatmaja@gmail.com>
2022-06-20 13:50:59 -0700

    error

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <string.h>
 
-/*
+/**
  * _strchr - function that locates a character in a string.
- * @s: the string to search
- * @c: the character to find
- * Return: pointer to the first occurrence of the character
+ * @s: the string to be scanned
+ * @c: the character to be searched
+ * Return: a pointer to the first occurrence of the character
  */
 
 char *_strchr(char *s, char c)
