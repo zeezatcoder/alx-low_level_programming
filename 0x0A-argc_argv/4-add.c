0a44f9c4baa322e0386456720e512786eb19ad5e
zeezatcoder <azeezatmaja@gmail.com>
2022-06-24 07:18:06 -0700

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that multiplies two numbers.
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success;
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc <= 2)
+		printf("Error\n");
+
+	else
+		printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
+	return (0);
+}
