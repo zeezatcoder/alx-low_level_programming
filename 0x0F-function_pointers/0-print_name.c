f0d7d687776654b0c3d6498f115547c7d9ca0dda
zeezatcoder <azeezatmaja@gmail.com>
2022-07-06 10:24:50 -0700

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - Prints name using functions pointers
+ * @name: first  argument of the function and pointer
+ * @f: second argument and function pointer
+ * Return:NOTHING
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	void (*ptr)(char *);
+
+	if (f != NULL)
+	{
+		ptr = f;
+		ptr(name);ptr(name);
+	}
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stdio.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int  int_index(int *array, int size, int (*cmp)(int));
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+#endif
+
